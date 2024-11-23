a06c8c0e7714cf1c4dc80a7f7f93641ed9bc9cb6
Heinrich <gui.heinrich1@gmail.com>
2024-11-23 00:42:58 -0300

    CRUD de gerenciamento de produtos, feito com Spring Boot.

---
+++ src/main/java/com/bigas/FirstStep/repositories/ProductRepository.java
@@ -0,0 +1,14 @@
+package com.bigas.FirstStep.repositories;
+
+
+import com.bigas.FirstStep.models.ProductModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductModel, UUID> {
+
+
+}
